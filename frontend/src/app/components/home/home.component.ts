cf00929120877b833fae2c0963919c1d5c6c52d2
Sourabh P <sourabhp94503@gmail.com>
2024-03-24 10:18:44 +0530

    Update: Objectives, GetStarted, Carousel

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,68 +1,93 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   images = [
-    { src: 'assets/slider11.png', title: '' },
-    { src: 'assets/slider2.png', title: '' },
+    { src: '/assets/slider1.png', title: '' },
+    { src: '/assets/slider2.png', title: '' },
+    { src: '/assets/slider3.png', title: '' },
+  ]
 
-  ];
-
-  currentSlide = 0;
-  slideWidth!: number;
-  timer: any;
-
-  @ViewChild('carousel') carousel!: ElementRef<HTMLDivElement>;
+  currentSlide = 0
+  timer: any
+  carouselImage: any = null
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   ngAfterViewInit() {
-    this.slideWidth = this.carousel.nativeElement.clientWidth;
-    this.carousel.nativeElement.style.transform = `translateX(0px)`;
-    this.startSlider();
+    this.carouselImage = document.getElementById('carousel-image')
+    this.startSlider()
   }
 
   ngOnDestroy() {
-    clearInterval(this.timer);
+    clearInterval(this.timer)
   }
 
   startSlider() {
     this.timer = setInterval(() => {
-      this.next();
-    }, 3000); // Change slide every 3 seconds
+      this.next()
+    }, 3000)
   }
 
   next(): void {
     if (this.currentSlide < this.images.length - 1) {
-      this.currentSlide++;
+      if (this.carouselImage == null) {
+        this.currentSlide++
+      } else {
+        this.carouselImage.style.opacity = 0 // Fade out
+        setTimeout(() => {
+          this.currentSlide++
+          this.carouselImage.setAttribute('src', this.images[this.currentSlide].src)
+          this.carouselImage.style.opacity = 1 // Fade in
+        }, 300)
+      }
     } else {
-      this.currentSlide = 0; // Reset to the first slide if at the end
+      if (this.carouselImage == null) {
+        this.currentSlide = 0
+      } else {
+        this.currentSlide = 0
+        this.carouselImage.style.opacity = 0 // Fade out
+        setTimeout(() => {
+          this.carouselImage.setAttribute('src', this.images[this.currentSlide].src)
+          this.carouselImage.style.opacity = 1 // Fade in
+        }, 300)
+      }
     }
-    this.updateCarousel();
   }
 
   prev(): void {
     if (this.currentSlide > 0) {
-      this.currentSlide--;
+      if (this.carouselImage == null) {
+        this.currentSlide--
+      } else {
+        this.carouselImage.style.opacity = 0 // Fade out
+        setTimeout(() => {
+          this.currentSlide--
+          this.carouselImage.setAttribute('src', this.images[this.currentSlide])
+          this.carouselImage.style.opacity = 1 // Fade in
+        }, 300)
+      }
     } else {
-      this.currentSlide = this.images.length - 1; // Go to the last slide if at the beginning
+      if (this.carouselImage == null) {
+        this.images.length - 1
+      } else {
+        this.images.length - 1
+        this.carouselImage.style.opacity = 0 // Fade out
+        setTimeout(() => {
+          this.carouselImage.setAttribute('src', this.images[this.currentSlide])
+          this.carouselImage.style.opacity = 1 // Fade in
+        }, 300)
+      }
     }
-    this.updateCarousel();
   }
 
-  updateCarousel(): void {
-    if (this.carousel && this.carousel.nativeElement) {
-      const newTransform = -this.currentSlide * this.slideWidth;
-      this.carousel.nativeElement.style.transform = `translateX(${newTransform}px)`;
-    }
-  }
 }
