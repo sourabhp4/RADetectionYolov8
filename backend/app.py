be0fd7aaf5db73351fabc1b119f85f51c134fac3
Sourabh P <sourabhp94503@gmail.com>
2024-08-04 10:57:03 +0530

    Update: MONGODB Path

--- backend/app.py
+++ backend/app.py
@@ -88,7 +88,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configuration for MongoDB connection
-client = MongoClient(os.environ.get('MONGO_URI'))
+client = MongoClient(os.environ.get('MONGO_URI_CLUSTER'))
 db = client['godproject']
 users_collection = db['users']
 predictions_collection = db['predictions']
